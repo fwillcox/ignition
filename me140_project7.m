e5f134fcdbbbca740f707ce6ad0905dc7885de68
Andrew Lovett <alovett@stanford.edu>
2016-05-10 11:15:04 -0700

    comment

--- me140_project7.m
+++ me140_project7.m
@@ -4,6 +4,7 @@
 % NOTE: All code for this project should be able to deal with effects of 
 % both (1) chemical equilibrium and (2) frozen flow in nozzle assumptions
 
+%kdkdkd
 % ------
 % TODOS:
 % ------
